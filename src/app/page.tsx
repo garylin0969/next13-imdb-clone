e84926d66626723e785f4af16d4a8a7d64705f92
Gary <garylin0969@gmail.com>
2023-03-26 14:15:44 +0800

    install tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] });
 export default function Home() {
     return (
         <>
-            <h1>Home</h1>
+            <h1 className="text-red-400">Home</h1>
         </>
     );
 }
