58470b33d6bbeed03fb323560af9fab8c6cb7a91
Gary <garylin0969@gmail.com>
2023-03-26 20:37:21 +0800

    create the navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header';
-import './globals.css';
+import Navbar from '@/components/Navbar';
 import Providers from './Providers';
+import './globals.css';
 
 export const metadata = {
     title: 'IMDb Clone',
@@ -13,6 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <body>
                 <Providers>
                     <Header />
+                    <Navbar />
                     {children}
                 </Providers>
             </body>
---
+++ src/components/NavbarItem.tsx
@@ -0,0 +1,30 @@
+'use client';
+import Link from 'next/link';
+import { ReadonlyURLSearchParams, useSearchParams } from 'next/navigation';
+
+interface NavbarItemProps {
+    title: string;
+    param: string;
+}
+
+const NavbarItem = ({ title, param }: NavbarItemProps) => {
+    const searchParams: ReadonlyURLSearchParams = useSearchParams();
+    const genre: string | null = searchParams.get('genre');
+
+    return (
+        <div>
+            <Link
+                href={`/?genre=${param}`}
+                className={`m-4 hover:text-amber-600 font-semibold p-2 ${
+                    genre &&
+                    genre === param &&
+                    'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
+                }`}
+            >
+                {title}
+            </Link>
+        </div>
+    );
+};
+
+export default NavbarItem;
