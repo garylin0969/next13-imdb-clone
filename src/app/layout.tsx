09db2bcc72caab23e3b494ae5d7ad9a561b126c9
Gary <garylin0969@gmail.com>
2023-03-30 23:53:49 +0800

    revise: 1. Import SearchBox

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,24 @@
 import Header from '@/components/Header';
 import Navbar from '@/components/Navbar';
+import SearchBox from '@/components/SearchBox';
+import { Metadata } from 'next/types';
+import { ReactNode } from 'react';
 import Providers from './Providers';
 import './globals.css';
 
-export const metadata = {
+export const metadata: Metadata = {
     title: 'IMDb Clone',
     description: 'This i s the IMDb Clone website',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html lang="en">
             <body>
                 <Providers>
                     <Header />
                     <Navbar />
+                    <SearchBox />
                     {children}
                 </Providers>
             </body>
