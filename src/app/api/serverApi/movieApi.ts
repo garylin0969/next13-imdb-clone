65007c9da4ccf6c5b96ad6f7a2661795d30e12cc
Gary <garylin0969@gmail.com>
2023-03-31 00:00:48 +0800

    feat: develop movie api

---
+++ src/app/api/serverApi/movieApi.ts
@@ -0,0 +1,22 @@
+import { MovieInfo, MoviesResponse } from '@/types';
+import { revalidateFetching, staticFetching } from './basicFetching';
+
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
+const showMovies = async (genre?: string): Promise<MoviesResponse> => {
+    const isTopRated: string = genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/all/week';
+    const params: object = { api_key: API_KEY, language: 'en-US', page: 1 };
+    return await revalidateFetching(`/${isTopRated}`, 1000, params);
+};
+
+const getMovie = async (movieId: string): Promise<MovieInfo> => {
+    const params: object = { api_key: API_KEY };
+    return await staticFetching(`/movie/${movieId}`, params);
+};
+
+const searchMovies = async (search: string): Promise<MoviesResponse> => {
+    const params: object = { api_key: API_KEY, query: search, language: 'en-US', include_adult: false };
+    return await staticFetching('/search/movie', params);
+};
+
+export { showMovies, getMovie, searchMovies };
