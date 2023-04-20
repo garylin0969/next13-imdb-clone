0cde9660aa70da75032a1ef8701e9f0650ffaa39
Gary <garylin0969@gmail.com>
2023-04-20 21:44:27 +0800

    refactor: refactor form with react-hook-form

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -2,27 +2,25 @@
 
 import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context';
 import { useRouter } from 'next/navigation';
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { FieldValues, useForm } from 'react-hook-form';
 
 const SearchBox = () => {
     const router: AppRouterInstance = useRouter();
-    const [search, setSearch] = useState<string>('');
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (!search) return;
-        router.push(`/search/${search}`);
+    const { register, handleSubmit, watch } = useForm();
+    const submit = (data: FieldValues): void => {
+        if (!data.search.trim()) return;
+        router.push(`/search/${data.search.trim()}`);
     };
     return (
         <>
-            <form onSubmit={handleSubmit} className="flex max-w-6xl mx-auto justify-between items-center px-5">
+            <form className="flex max-w-6xl mx-auto justify-between items-center px-5" onSubmit={handleSubmit(submit)}>
                 <input
                     className="w-full h-14 rounded-sm placeholder-gray-500 outline-none bg-transparent flex-1"
                     type="text"
-                    value={search}
+                    {...register('search')}
                     placeholder="Search keywords..."
-                    onChange={(e: ChangeEvent<HTMLInputElement>) => setSearch(e.target.value)}
                 />
-                <button type="submit" disabled={!search} className="text-amber-600 disabled:text-gray-400">
+                <button className="text-amber-600 disabled:text-gray-400" type="submit" disabled={!watch('search')}>
                     Search
                 </button>
             </form>
